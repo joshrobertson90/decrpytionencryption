dc2fe30b2a0860257b040e49be648e14bc3a36c4
joshrobertson90 <45882786+joshrobertson90@users.noreply.github.com>
2019-01-29 13:49:02 +0000

    V1

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Build an encryption program that uses the shift by n algorithm to generate a key
+// to decrypt and encrypt a message.
+
+// function prototypes
+void encrpyt(char [], int);
+void decrpyt(char [], int);
+
+int main()
+{
+ char CrytoMessage[21] = 0; // variable for message
+ int key;  // variable for key
+ int Select = 0; // variable to select option
+
+
+ // Offer a menu system
+ // 1 to enter text to encrypt
+ // 2 to enter text to decrypt
+ // 3 to create a new key
+ // 4 to quit
+
+
+}
+
+void encrypt(//choose inputs)
+{
+    int x;
+    //encrypt the message by shifting each characters ASCII value according to the key generated
+
+}
+
+void decrypt(//choose inputs)
+{
+    int x = 0;
+    // decrypt the message by shifting each characters ASCII value according to the key generated
+}
